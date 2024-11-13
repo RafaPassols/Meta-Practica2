0920f3245dc921d0f07a3740582a7ed30cadccd7
Rafa Passols <rcp00054@red.ujaen.es>
2024-11-13 19:57:54 +0100

    Algoritmos ya funcionales y optimizados (todo lo posible)

---
+++ algoritmos/AlgGRE_Clase01_Grupo06.py
@@ -0,0 +1,62 @@
+# algoritmos/AlgGRE_Clase01_Grupo06.py
+
+# Importaciones de bibliotecas estándar
+import random
+
+# Importaciones de terceros
+import numpy as np
+
+
+class GreedyAleatorio:
+    """Implementa el algoritmo greedy aleatorio para la generación de soluciones"""
+
+    def __init__(self, matriz, k):
+        self.matriz = matriz
+        self.k = k
+
+        if not (1 <= k <= self.matriz.shape[0]):
+            raise ValueError("El valor de 'k' debe estar entre 1 y el número total de ciudades.")
+
+
+    def ejecutar(self):
+        tour = []
+        num_ciudades = self.matriz.shape[0]
+        visitadas = np.zeros(num_ciudades, dtype=bool)
+        rng = np.random.default_rng()
+
+        # Calcula la suma de cada ciudad al resto
+        suma_distancias = np.sum(self.matriz, axis=1)
+
+        # Ordena las ciudades
+        ciudades_ordenadas = np.argsort(suma_distancias)
+
+        # Selecciona la ciudad inicial de entre las 'k' más prometedoras
+        prometedoras = ciudades_ordenadas[:self.k]
+        ciudad_actual = rng.choice(prometedoras)
+
+        # Añade la ciudad a la solucion
+        tour.append(ciudad_actual)
+        visitadas[ciudad_actual] = True
+        distancia_total = 0.0
+
+        for _ in range(num_ciudades - 1):
+            # Filtra las ciudades no visitadas y obtiene las 'k' más prometedoras
+            no_visitadas = ciudades_ordenadas[~visitadas[ciudades_ordenadas]]
+            prometedoras = no_visitadas[:self.k]
+
+            # Elige una ciudad aleatoriamente
+            ciudad_siguiente = rng.choice(prometedoras)
+            distancia_total += self.matriz[ciudad_actual, ciudad_siguiente]
+
+            # Actualiza la solución
+            tour.append(ciudad_siguiente)
+            visitadas[ciudad_siguiente] = True
+            ciudad_actual = ciudad_siguiente
+
+        # Regreso a la ciudad de inicio
+        distancia_total += self.matriz[ciudad_actual, tour[0]]
+
+        # Convierte tour a un array de numpy
+        tour = np.asarray(tour)
+
+        return tour, distancia_total
--- main.py
+++ main.py
@@ -1,19 +1,135 @@
-import sys
+# main.py
+import multiprocessing
+# Importaciones de bibliotecas estándar
+import sys, os, random
+import time
+import multiprocessing as mp
 
-from utils.procesar_configuracion import Configuracion
+# Importaciones locales
+from auxiliares.funciones_generales import generar_semillas
+from auxiliares.procesador_archivos import ProcesadorTXT, ProcesadorTSP
+from algoritmos.evolutivos import Generacional
+from algoritmos.evolutivos import Estacionario
 
+# Importaciones de terceros
+import numpy as np
+
+
+def print_hi(name):
+    """Muestra un mensaje de bienvenida."""
+    print(f'👋 ¡Hola, {name}! Bienvenido al mundo de las soluciones optimizadas. 🚀🧬\n')
+
+
+def procesar_config(archivo : str) -> dict:
+    txt = ProcesadorTXT(archivo)
+    return txt.cargar_datos_txt()
+
+
+def ejecutar_generacional(matriz, params, queue: mp.Queue = None):
+    generacional = Generacional(matriz, params)
+    generacional.ejecutar()
+    result = (
+        f'GENERACIONAL\n'
+        f'Generación final alcanzada: {generacional.generacion}\n'
+        f'Evaluaciones realizadas: {generacional.num_evaluaciones}\n'
+        f'Tiempo de ejecucion: {generacional.tiempo_ejecucion}\n'
+        f'Mejor individuo: {min(generacional.poblacion, key=lambda ind: ind.distancia)}\n\n'
+    )
+    if queue is not None:
+        queue.put(result)
+    return result
+
+def ejecutar_estacionario(matriz, params, queue: mp.Queue = None) -> str:
+    estacionario = Estacionario(matriz, params)
+    estacionario.ejecutar()
+    result = (
+        f'ESTACIONARIO\n'
+        f'Generación final alcanzada: {estacionario.generacion}\n'
+        f'Evaluaciones realizadas: {estacionario.num_evaluaciones}\n'
+        f'Tiempo de ejecucion: {estacionario.tiempo_ejecucion}\n'
+        f'Mejor individuo: {min(estacionario.poblacion, key=lambda ind: ind.distancia)}\n\n'
+    )
+    if queue is not None:
+        queue.put(result)
+    return result
+
+
+def procesar_archivos_tsp(archivos_tsp, params, semillas) -> list[str]:
+    resultados: list[str] = []
+    for archivo in archivos_tsp:
+        # Los archivos estarán guardados en una carpeta "data"
+        ruta_archivo = os.path.join('data', archivo)
+
+        if not os.path.exists(ruta_archivo):
+            print(f'Error: El archivo {ruta_archivo} no se encontró.')
+            continue
+
+        print('========================')
+        print(f'Procesando {archivo}:')
+        print('========================')
+
+        matriz = ProcesadorTSP(ruta_archivo).cargar_datos_tsp()
+        for semilla in semillas:
+            random.seed(semilla)
+            np.random.seed(semilla)
+
+            processes = []
+            queue = mp.Queue()
+
+            if 'generacional' in params['algoritmos']:
+                processes.append(mp.Process(target=ejecutar_generacional, args=(matriz, params, queue)))
+            if 'estacionario' in params['algoritmos']:
+                processes.append(mp.Process(target=ejecutar_estacionario, args=(matriz, params, queue)))
+
+            for p in processes:
+                p.start()
+            for p in processes:
+                p.join()
+
+
+            # Guardar resultados
+            resultado = (
+                f'--------------------------------------------------\n'
+                f'Archivo: {archivo}    Semilla: {semilla}\n'
+                f'--------------------------------------------------\n'
+            )
+            while not queue.empty():
+                resultado += queue.get()
+            resultados.append(resultado)
+
+        print(f'Terminado {archivo}\n')
+
+    return resultados
 
 def main():
+    """Función principal"""
+    inicio = time.time()
+
+    # Comprueba que se pasen dos argumentos
     if len(sys.argv) != 2:
-        print("Uso: python ./main.py ./config.txt")
+        print('Uso: (python | py) ./main.py ./config.txt')
         sys.exit(1)
 
-    # Procesar configuración
-    archivo_configuracion = sys.argv[1]
-    params = Configuracion(archivo_configuracion).procesar()
+    params = procesar_config(sys.argv[1])
+    print('Parámetros procesados:')
+    for clave, valor in params.items():
+        print(f'{clave}: {valor}')
 
-    for key in params:
-        print(f'{key} -> {params[key]}')
+    # Genera las semillas pseudoaleatorias
+    semillas = generar_semillas(params['dni_alumno'], params['num_ejecuciones'])
+    print(f'n.º de semillas: {params["num_ejecuciones"]}')
+    print(f'semillas: {semillas}\n')
 
-if __name__ == "__main__":
+    # Procesa los archivos tsp
+    archivos_tsp = params['archivos_tsp']
+    results = procesar_archivos_tsp(archivos_tsp, params, semillas)
+    for result in results:
+        print(result)
+
+    final = time.time()
+    print(f'Tiempo de ejecución del programa: {final-inicio}')
+
+
+if __name__ == '__main__':
+    print_hi('Cristobal')
     main()
